6f2dadd4386b99e29e546de30e1117c25fd72f9e
Eli Esrig <elazare@brandeis.edu>
2018-10-29 16:18:53 -0400

    Added a semaphore

--- src/cs131/pa2/CarsTunnels/BasicTunnel.java
+++ src/cs131/pa2/CarsTunnels/BasicTunnel.java
@@ -1,21 +1,38 @@
 package cs131.pa2.CarsTunnels;
 
+import cs131.pa2.Abstract.Direction;
 import cs131.pa2.Abstract.Tunnel;
 import cs131.pa2.Abstract.Vehicle;
 
 public class BasicTunnel extends Tunnel{
-
+	private int slots;
+	private Direction direction;
+	
 	public BasicTunnel(String name) {
 		super(name);
+		slots = 3;
+		direction = null;
 	}
 
 	@Override
-	public boolean tryToEnterInner(Vehicle vehicle) {
-		return false;
+	public synchronized boolean tryToEnterInner(Vehicle vehicle) {
+		if(vehicle instanceof Sled) {
+			if(slots >= 3) {
+				slots -= 3;
+				return true;
+			}else {
+				return false;
+			}
+		}else if(vehicle instanceof Car) {
+			return false;
+		}else {
+			//TODO: Add some stuff here
+			return false;
+		}
 	}
 
 	@Override
-	public void exitTunnelInner(Vehicle vehicle) {
+	public synchronized void exitTunnelInner(Vehicle vehicle) {
 		
 	}
 	
